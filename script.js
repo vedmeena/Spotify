00f1cda110d56a96d8259b2672cc0e50cf789eae
vedmeena <118125165+vedmeena@users.noreply.github.com>
2022-11-13 22:33:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("welcome to spotify");
+let songs = [
+  {songName: "aashiqi aa gayi", filePath: "spootify/No-Love.mp3",coverPath:"spootify/cover.jpg"}
+]
+let audioElement = new Audio('No-Love.mp3');
